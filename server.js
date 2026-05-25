9b56637d1a9f52d6cb0b000ddaa534ac76016da2
vankalamoulika2004-bit <vankalamoulika2004@gmail.com>
2026-05-25 12:10:46 +0530

    CHANGES

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 
 const connectDB = require("./config/db");
-const userRoutes = require("./routes/userRoutes");
+const userRoutes = require("./routes/UserRoutes");
 const dns = require("dns");
 dns.setServers(["8.8.8.8", "1.1.1.1"]);
 const cors = require("cors");
